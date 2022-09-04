7c760bd2bf91cefbf58445e9e016fa2b39bb714e
Kkthnx <40672673+Kkthnx@users.noreply.github.com>
2022-09-04 15:46:57 -0400

    Update GUI.lua

--- KkthnxUI/Config/GUI.lua
+++ KkthnxUI/Config/GUI.lua
@@ -16,7 +16,7 @@ local TUTORIAL_TITLE47 = _G.TUTORIAL_TITLE47
 
 local emojiExampleIcon = "|TInterface\\Addons\\KkthnxUI\\Media\\Chat\\Emojis\\StuckOutTongueClosedEyes:0:0:4|t"
 local enableTextColor = "|cff00cc4c"
-local newFeatureIcon = "|TInterface\\GossipFrame\\CampaignAvailableQuestIcon:16:16:-2|t"
+local newFeatureIcon = "|TInterface\\OptionsFrame\\UI-OptionsFrame-NewFeatureIcon:14:14:-2|t"
 
 local function updateBagSize()
 	K:GetModule("Bags"):UpdateBagSize()
